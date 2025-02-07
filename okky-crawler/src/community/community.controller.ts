a2fecc34bb307d431c0b0674977b716d45862919
smw0807 <smw0807@gmail.com>
2025-02-07 11:01:02 +0900

    로그인 로직 추가

--- okky-crawler/src/community/community.controller.ts
+++ okky-crawler/src/community/community.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Get, Logger, Res } from '@nestjs/common';
 import { CommunityService } from './community.service';
 import { Response } from 'express';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('community')
 export class CommunityController {
   private readonly logger = new Logger(CommunityController.name);
-  constructor(private readonly communityService: CommunityService) {}
+  constructor(
+    private readonly communityService: CommunityService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   async getCommunity(@Res() res: Response) {
     try {
+      await this.authService.login();
       await this.communityService.crawl();
       res.send('success');
     } catch (e) {
