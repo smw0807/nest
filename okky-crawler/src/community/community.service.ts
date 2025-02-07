df67c32343cad00dbb6671fe2c8c04fb22d5946d
smw0807 <smw0807@gmail.com>
2025-02-07 21:57:08 +0900

    임시 커밋

--- okky-crawler/src/community/community.service.ts
+++ okky-crawler/src/community/community.service.ts
@@ -1,22 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { OKKY_COMMUNITY_URL } from '../constants/url';
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
 @Injectable()
 export class CommunityService {
   private readonly logger = new Logger(CommunityService.name);
   constructor() {}
 
-  async crawl() {
+  async crawl(page: Page) {
     const browser = await puppeteer.launch({ headless: false });
     try {
-      const page = await browser.newPage();
       await page.goto(OKKY_COMMUNITY_URL);
+
       // const content = await page.content();
       // console.log(content);
-      const $ = await page.$$eval('#__next', (elements) => {
-        console.log(elements);
-      });
+      // const $ = await page.$$eval('#__next', (elements) => {
+      //   console.log(elements);
+      // });
       // console.log($);
       // const $ = cheerio.load(content);
       // const articles = $(
