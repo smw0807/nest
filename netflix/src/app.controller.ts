7ac85541d4db8f7520601a3756770d0315337483
smw0807 <smw0807@gmail.com>
2025-02-10 00:17:13 +0900

    코드팩토리 강의 - 프보바이더, 서비스

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   Patch,
   Post,
@@ -21,79 +20,36 @@ interface Movie {
   director: string;
   cast: string[];
 }
+/**
+ * Controller에서는 들어오는 요청에 대한 프로세싱만 진행한다.
+ * 비즈니스 로직은 서비스 클래스에 작성한다.
+ */
 @Controller('movie')
 export class AppController {
-  private movies: Movie[] = [
-    {
-      id: 1,
-      name: 'The Shawshank Redemption',
-      description:
-        'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-      rating: 9.3,
-      year: 1994,
-      genre: 'Drama',
-      director: 'Frank Darabont',
-      cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
-    },
-    {
-      id: 2,
-      name: 'The Godfather',
-      description:
-        'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-      rating: 9.2,
-      year: 1972,
-      genre: 'Crime, Drama',
-      director: 'Francis Ford Coppola',
-      cast: ['Marlon Brando', 'Al Paccino', 'James Caan'],
-    },
-  ];
-  private idCounter = 3;
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getMovies(@Query('name') name: string) {
-    if (!name) {
-      return this.movies;
-    }
-    return this.movies.filter((movie) => movie.name.includes(name));
+    return this.appService.getManyMovies(name);
   }
 
   @Get(':id')
   getMovie(@Param('id') id: string) {
-    const movie = this.movies.find((movie) => movie.id === +id);
-    if (!movie) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
-    }
-    return movie;
+    return this.appService.getMovieById(+id);
   }
 
   @Post()
   postMovie(@Body() movie: Movie) {
-    const newMovie = {
-      id: this.idCounter++,
-      ...movie,
-    };
-    this.movies.push(newMovie);
-    return newMovie;
+    return this.appService.createMovie(movie);
   }
 
   @Patch(':id')
   patchMovie(@Param('id') id: string, @Body() movie: Movie) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-    if (movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
-    }
-    Object.assign(this.movies[movieIndex], movie);
-    return this.movies[movieIndex];
+    return this.appService.updateMovie(+id, movie);
   }
 
   @Delete(':id')
   deleteMovie(@Param('id') id: string) {
-    const movieIndex = this.movies.findIndex((movie) => movie.id === +id);
-    if (movieIndex === -1) {
-      throw new NotFoundException('존재하지 않는 ID의 영화입니다.');
-    }
-    this.movies.splice(movieIndex, 1);
-    return id;
+    return this.appService.deleteMovie(+id);
   }
 }
