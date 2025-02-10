fca57c74b87330ae1eb951fb0f8c86548bf9d6d4
smw0807 <smw0807@gmail.com>
2025-02-10 20:06:12 +0900

    필요없는 파일 삭제

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
 
 @Module({
   imports: [MovieModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
