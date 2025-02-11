1d8df58d98180c490961caff3edde43c232f9ccb
smw0807 <smw0807@gmail.com>
2025-02-11 22:15:04 +0900

    코드팩토리 강의 - class-validator

--- netflix/src/movie/dto/update-movie.dto.ts
+++ netflix/src/movie/dto/update-movie.dto.ts
@@ -1,9 +1,25 @@
+import { IsOptional, IsString, IsNumber, IsArray } from 'class-validator';
+
 export class UpdateMovieDto {
+  @IsOptional()
+  @IsString()
   name?: string;
+  @IsOptional()
+  @IsString()
   description?: string;
+  @IsOptional()
+  @IsNumber()
   rating?: number;
+  @IsOptional()
+  @IsNumber()
   year?: number;
+  @IsOptional()
+  @IsString()
   genre?: string;
+  @IsOptional()
+  @IsString()
   director?: string;
+  @IsOptional()
+  @IsArray()
   cast?: string[];
 }
