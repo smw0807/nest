22fff2a3fa4bd1f2b541f1e2da2dfc09292eb6ae
smw0807 <smw0807@gmail.com>
2025-02-12 20:40:41 +0900

    validationPipe 수정

--- netflix/src/main.ts
+++ netflix/src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // 정의되지 않은 프로퍼티가 있으면 제거함
+      whitelist: true,
+      // 정의되지 않은 프로퍼티가 있으면 오류 발생
+      forbidNonWhitelisted: true,
+      // 데이터 타입이 다르면 오류 발생
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
