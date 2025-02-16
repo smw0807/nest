b0d837cf1f82c0ae1b59447f5404c82f0aec0787
smw0807 <smw0807@gmail.com>
2025-02-16 19:39:30 +0900

    코드팩토리 강의 - OneToOne 관계

--- netflix/src/movie/entity/movie.entity.ts
+++ netflix/src/movie/entity/movie.entity.ts
@@ -2,24 +2,21 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
+import { BaseTable } from './base-table.entity';
+import { MovieDetail } from './movie-detail.entity';
 
-export class BaseEntity {
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @UpdateDateColumn()
-  updatedAt: Date;
-
-  @VersionColumn()
-  version: number;
-}
+/// Many to One - Director -> 감독은 여러개의 영화를 만들 수 있음
+// One To One - MovieDetail -> 영화는 하나의 상세 내용을 갖을 수 있음
+/// Many To Many - Genre -> 영화는 여러개의 장르를 갖을 수 있고 장르는 여러개의 영화에 속할 수 있음
 
 @Entity()
-export class Movie extends BaseEntity {
+export class Movie extends BaseTable {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -43,6 +40,10 @@ export class Movie extends BaseEntity {
 
   @Column('text', { array: true })
   actors: string[];
+
+  @OneToOne(() => MovieDetail)
+  @JoinColumn()
+  detail: MovieDetail;
 
   // @Column(() => BaseEntity)
   // base: BaseEntity;
