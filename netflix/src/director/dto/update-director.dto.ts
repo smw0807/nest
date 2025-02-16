15a809db4a1597f43efaff22a98fcdbd399c6135
smw0807 <smw0807@gmail.com>
2025-02-17 08:23:32 +0900

    소스정리

--- netflix/src/director/dto/update-director.dto.ts
+++ netflix/src/director/dto/update-director.dto.ts
@@ -1,5 +1,3 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDirectorDto } from './create-director.dto';
 import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateDirectorDto {
