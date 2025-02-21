4b45c039347ef41d0381c4f5f762bc6e37c2723c
smw0807 <smw0807@gmail.com>
2025-02-21 16:19:11 +0900

    코드팩토리 강의 - 로그인

--- netflix/src/auth/auth.module.ts
+++ netflix/src/auth/auth.module.ts
@@ -4,9 +4,14 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
