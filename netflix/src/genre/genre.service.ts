265807384edae786c733fbd0cca43d08a9e6ed61
smw0807 <smw0807@gmail.com>
2025-02-24 14:14:44 +0900

    수정

--- netflix/src/genre/genre.service.ts
+++ netflix/src/genre/genre.service.ts
@@ -20,7 +20,13 @@ export class GenreService {
     return this.genreRepository.findOne({ where: { id } });
   }
 
-  create(createGenreDto: CreateGenreDto) {
+  async create(createGenreDto: CreateGenreDto) {
+    const genre = await this.genreRepository.findOne({
+      where: { name: createGenreDto.name },
+    });
+    if (genre) {
+      throw new NotFoundException('이미 존재하는 장르입니다.');
+    }
     return this.genreRepository.save(createGenreDto);
   }
 
