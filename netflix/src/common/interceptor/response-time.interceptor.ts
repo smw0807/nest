856e86264af5c56061717fb40ad77bd80bf42e4d
smw0807 <smw0807@gmail.com>
2025-02-26 08:13:15 +0900

    코드팩토리 강의 - Response Time Interceptor

---
+++ netflix/src/common/interceptor/response-time.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  NestInterceptor,
+} from '@nestjs/common';
+import { delay, Observable, tap } from 'rxjs';
+
+@Injectable()
+export class ResponseTimeInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+
+    const reqTime = Date.now();
+    return next.handle().pipe(
+      delay(1000),
+      tap(() => {
+        const resTime = Date.now();
+        const diff = resTime - reqTime;
+
+        if (diff > 1000) {
+          console.log(`!!!TIME OUT!!! [${req.method} ${req.path}] ${diff}ms`);
+          throw new InternalServerErrorException(
+            '시간이 너무 오래 걸렸습니다.',
+          );
+        } else {
+          console.log(`[${req.method} ${req.path}] ${diff}ms`);
+        }
+      }),
+    );
+  }
+}
