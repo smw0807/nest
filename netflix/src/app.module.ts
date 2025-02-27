b4eeb7f97036ffaca710f513b884da87617b30ea
smw0807 <smw0807@gmail.com>
2025-02-27 14:45:13 +0900

    코드팩토리 강의 - Query Failed Exception Filter

--- netflix/src/app.module.ts
+++ netflix/src/app.module.ts
@@ -24,6 +24,7 @@ import { AuthGuard } from './auth/guard/auth.guard';
 import { RBACGuard } from './auth/guard/rbac.guard';
 import { ResponseTimeInterceptor } from './common/interceptor/response-time.interceptor';
 import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
+import { QueryFailedExceptionFilter } from './common/filter/query-failed.filter';
 
 @Module({
   imports: [
@@ -79,6 +80,10 @@ import { ForbiddenExceptionFilter } from './common/filter/forbidden.filter';
     {
       provide: APP_FILTER,
       useClass: ForbiddenExceptionFilter,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: QueryFailedExceptionFilter,
     },
   ],
 })
---
+++ netflix/src/common/filter/query-failed.filter.ts
@@ -0,0 +1,26 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+
+@Catch(QueryFailedError)
+export class QueryFailedExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status = 400;
+
+    let message = '데이터베이스 에러!';
+
+    if (exception.message.includes('duplicate')) {
+      message = '이미 존재하는 데이터입니다.';
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
