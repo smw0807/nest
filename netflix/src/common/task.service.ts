de8644841b592193ae2feff3dffad18444eca120
smw0807 <smw0807@gmail.com>
2025-03-04 06:28:36 +0900

    코드팩토리 강의 - 잉여 파일 삭제 기능 구현하기

--- netflix/src/common/task.service.ts
+++ netflix/src/common/task.service.ts
@@ -1,12 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { readdir, unlink } from 'fs/promises';
+import { join, parse } from 'path';
 
 @Injectable()
 export class TaskService {
   constructor() {}
 
-  @Cron('* * * * * *')
+  // @Cron('* * * * * *')
   logEverySecond() {
     console.log('1초 마다 실행');
+  }
+
+  // @Cron('* * * * * *')
+  async eraseOrphanFiles() {
+    const files = await readdir(join(process.cwd(), 'public', 'temp'));
+
+    const deleteFileTargets = files.filter((file) => {
+      const filename = parse(file).name;
+      const split = filename.split('_');
+
+      if (split.length !== 2) {
+        return true;
+      }
+
+      try {
+        const date = +new Date(parseInt(split[split.length - 1]));
+        const aDayInMilSec = 24 * 60 * 60 * 1000;
+        const now = +new Date();
+        // 24시간 이상 지난 파일
+        return now - date > aDayInMilSec;
+      } catch (e) {
+        return true;
+      }
+    });
+
+    await Promise.all(
+      deleteFileTargets.map((file) =>
+        unlink(join(process.cwd(), 'public', 'temp', file)),
+      ),
+    );
   }
 }
