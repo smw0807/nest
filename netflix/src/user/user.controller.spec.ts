c64508fa892c8baf7b73f1d545ab7a72de26dd83
smw0807 <smw0807@gmail.com>
2025-03-09 11:18:45 +0900

    코드팩토리 강의 - User Controller 테스트

--- netflix/src/user/user.controller.spec.ts
+++ netflix/src/user/user.controller.spec.ts
@@ -1,20 +1,128 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+const mockUserService = {
+  create: jest.fn(),
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  update: jest.fn(),
+  remove: jest.fn(),
+};
 
 describe('UserController', () => {
-  let controller: UserController;
-
+  let userController: UserController;
+  let userService: UserService;
   beforeEach(async () => {
-    // const module: TestingModule = await Test.createTestingModule({
-    //   controllers: [UserController],
-    //   providers: [UserService],
-    // }).compile();
-    // controller = module.get<UserController>(UserController);
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
+    }).compile();
+    userController = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    // expect(controller).toBeDefined();
-    expect(true).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return correct value', async () => {
+      const createUserDto: CreateUserDto = {
+        email: 'smw@gmail.com',
+        password: '1234',
+      };
+      const user = {
+        id: 1,
+        ...createUserDto,
+        password: 'asdfgadsfgasdf',
+      };
+      jest.spyOn(userService, 'create').mockResolvedValue(user as User);
+
+      const result = await userController.create(createUserDto);
+
+      expect(userService.create).toHaveBeenCalledWith(createUserDto);
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return a list of users', async () => {
+      const users = [
+        {
+          id: 1,
+          email: 'smw@gmail.com',
+          password: '1234',
+        },
+        {
+          id: 2,
+          email: 'smw2@gmail.com',
+          password: '1234',
+        },
+      ];
+      jest.spyOn(userService, 'findAll').mockResolvedValue(users as User[]);
+
+      const result = await userController.findAll();
+
+      expect(userService.findAll).toHaveBeenCalled();
+      expect(result).toEqual(users);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a single user', async () => {
+      const user = {
+        id: 1,
+        email: 'smw@gmail.com',
+        password: '1234',
+      };
+      jest.spyOn(userService, 'findOne').mockResolvedValue(user as User);
+
+      const result = await userController.findOne(1);
+
+      expect(userService.findOne).toHaveBeenCalledWith(1);
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('update', () => {
+    it('should return the updated user', async () => {
+      const id = 1;
+      const updateUserDto: UpdateUserDto = {
+        email: 'smw@gmail.com',
+      };
+      const user = {
+        id,
+        ...updateUserDto,
+      };
+
+      jest.spyOn(userService, 'update').mockResolvedValue(user as User);
+
+      const result = await userController.update(1, updateUserDto);
+
+      expect(userService.update).toHaveBeenCalledWith(1, updateUserDto);
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('remove', () => {
+    it('should return a single user', async () => {
+      const id = 1;
+      jest.spyOn(userService, 'remove').mockResolvedValue(id);
+
+      const result = await userController.remove(1);
+
+      expect(userService.remove).toHaveBeenCalledWith(1);
+      expect(result).toEqual(id);
+    });
   });
 });
