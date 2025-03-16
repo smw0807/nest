c9ec370c779237db5a7006a7cc530f03e0681662
smw0807 <smw0807@gmail.com>
2025-03-16 14:12:20 +0900

    코드팩토리 강의 - End to End 테스트 세팅

---
+++ netflix/src/movie/movie.e2e.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from 'src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    await app.init();
+  });
+
+  describe('[GET /movie]', () => {
+    it('should get all movies', async () => {
+      const { body, statusCode, error } = await request(
+        app.getHttpServer(),
+      ).get('/movie');
+      console.log(error);
+      expect(statusCode).toBe(200);
+    });
+  });
+});
