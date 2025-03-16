858a96e5b2a3caeaee526e062bd9135f953144cb
smw0807 <smw0807@gmail.com>
2025-03-16 16:53:02 +0900

    코드팩토리 강의 - End to End 테스트 세팅2

--- netflix/src/movie/movie.e2e.spec.ts
+++ netflix/src/movie/movie.e2e.spec.ts
@@ -2,11 +2,26 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/app.module';
+import { DataSource } from 'typeorm';
+import { Director } from 'src/director/entity/director.entity';
+import { Movie } from './entity/movie.entity';
+import { Genre } from 'src/genre/entities/genre.entity';
+import { User } from 'src/user/entities/user.entity';
+import { MovieDetail } from './entity/movie-detail.entity';
+import { MovieUserLike } from './entity/movie-user-like.entity';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
-  beforeEach(async () => {
+  let users: User[];
+  let directors: Director[];
+  let movies: Movie[];
+  let genres: Genre[];
+
+  let token: string;
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -22,6 +37,74 @@ describe('AppController (e2e)', () => {
       }),
     );
     await app.init();
+
+    dataSource = app.get<DataSource>(DataSource);
+
+    const movieRepository = dataSource.getRepository(Movie);
+    const movieDetailRepository = dataSource.getRepository(MovieDetail);
+    const userRepository = dataSource.getRepository(User);
+    const directorRepository = dataSource.getRepository(Director);
+    const genreRepository = dataSource.getRepository(Genre);
+    const movieUserLikeRepository = dataSource.getRepository(MovieUserLike);
+
+    await movieRepository.delete({});
+    await movieDetailRepository.delete({});
+    await userRepository.delete({});
+    await directorRepository.delete({});
+    await genreRepository.delete({});
+    await movieUserLikeRepository.delete({});
+
+    users = [1, 2].map((x) =>
+      userRepository.create({
+        id: x,
+        email: `${x}@test.com`,
+        password: `123123`,
+      }),
+    );
+
+    await userRepository.save(users);
+
+    directors = [1, 2].map((x) =>
+      directorRepository.create({
+        id: x,
+        dob: new Date('1992-11-23'),
+        nationality: 'South Korea',
+        name: `Director Name ${x}`,
+      }),
+    );
+
+    await directorRepository.save(directors);
+
+    genres = [1, 2].map((x) =>
+      genreRepository.create({
+        id: x,
+        name: `Genre ${x}`,
+      }),
+    );
+
+    await genreRepository.save(genres);
+
+    movies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((x) =>
+      movieRepository.create({
+        id: x,
+        name: `Movie ${x}`,
+        creator: users[0],
+        genres: genres,
+        likeCount: 0,
+        dislikeCount: 0,
+        detail: movieDetailRepository.create({
+          detail: `Movie Detail ${x}`,
+        }),
+        movieFilePath: 'movies/movie1.mp4',
+        director: directors[0],
+        createdAt: new Date(`2023-9-${x}`),
+        rating: 5,
+        year: 2023,
+        actors: ['Song Min', 'Kim Min', 'Lee Min'],
+      }),
+    );
+
+    await movieRepository.save(movies);
   });
 
   describe('[GET /movie]', () => {
@@ -29,7 +112,6 @@ describe('AppController (e2e)', () => {
       const { body, statusCode, error } = await request(
         app.getHttpServer(),
       ).get('/movie');
-      console.log(error);
       expect(statusCode).toBe(200);
     });
   });
